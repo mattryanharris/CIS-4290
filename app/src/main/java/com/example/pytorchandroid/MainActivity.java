9cadcd85108d3e8226e629c71ee5dcc246244393
Matthew Harris <mrharris@jpl.nasa.gov>
2020-11-19 16:14:10 -0800

    Added change to make sure camera is working

--- app/src/main/java/com/example/pytorchandroid/MainActivity.java
+++ app/src/main/java/com/example/pytorchandroid/MainActivity.java
@@ -180,6 +180,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        initializeUiandCamera();
         int numCams = Camera.getNumberOfCameras();
         TextView txt=(TextView)findViewById(R.id.txtOverSv);
         txt.setText("");
